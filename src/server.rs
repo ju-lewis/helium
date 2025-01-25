8357fbcb6ed8a1738b65acf42469b8299f69844e
ju-lewis <boollewis04@gmail.com>
2025-01-25 12:48:52 +1100

    Implement basic threadpool task queue

--- src/server.rs
+++ src/server.rs
@@ -20,15 +20,35 @@ impl Server {
     }
 
 
-    fn create_thread_pool(thread_cap: u32, task_queue: Arc<Mutex<VecDeque<Box<dyn HeliumTask + Send>>>>) -> Vec<thread::JoinHandle<std::io::Result<()>>> {
+    fn create_thread_pool(thread_cap: u32, task_queue: Arc<Mutex<VecDeque<Box<dyn HeliumTask + Send>>>>) -> Vec<thread::JoinHandle<()>> {
     
         let mut thread_pool = Vec::new();
 
         for _ in 0..thread_cap {
-            thread_pool.push(thread::spawn(|| {
+    
+            let task_mutex = Arc::clone(&task_queue);
+
+            thread_pool.push(thread::spawn(move || {
                 /* Thread task handler */
-                
-                Ok(())
+                loop {
+                    let lock_res = task_mutex.lock();
+                    if lock_res.is_err() {
+                        return ();
+                    }
+                    let mut tasks = lock_res.unwrap();
+                    
+                    // Handle the first task in the queue
+                    let task = match tasks.pop_front() {
+                        None => continue,
+                        Some(t) => t
+                    };
+                    
+                    let task_result = task.execute();
+                    
+                    if task_result.is_err() {
+                        // Idk, log error or somethin
+                    }
+                }
             }));
         }
         
@@ -47,9 +67,9 @@ impl Server {
 }
 
 
-
+/// A HeliumTask represents the behaviour of tasks (handlers) for a Helium Server.
 pub trait HeliumTask {
-    
+    fn execute(&self) -> std::io::Result<()>;
 }
 
 
