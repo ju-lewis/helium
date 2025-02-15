5eab37d0420de2ba52be79cdc746bea528870d45
ju-lewis <julianlewis2023@gmail.com>
2025-02-15 12:07:19 +1100

    Start changing architecture to manage streams on main thread

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 
-use std::{any::Any, collections::{HashMap, VecDeque}, net::{TcpListener, TcpStream}, sync::{Arc, Mutex}, thread};
+use std::{any::Any, collections::{HashMap, VecDeque}, net::{TcpListener, TcpStream}, sync::{Arc, Mutex, mpsc::channel}, thread};
 use crate::http::{self, Method, Path, Query, Request, StatusCode};
 use crate::parsing::parse_http_request;
+use rand::random;
 
 use std::io::{Read, Write};
 
@@ -10,7 +11,9 @@ pub struct Server {
     max_threads: u32,
     thread_pool: Vec<thread::JoinHandle<()>>,
 
-    task_queue: Arc<Mutex<VecDeque<TcpStream>>>,
+    task_queue: Arc<Mutex<VecDeque<(u32, String)>>>,
+    streams: HashMap<u32, TcpStream>,
+    channel_rx: Option<std::sync::mpsc::Receiver<(u32, String)>>, // Receives (key, content) tuples
     handlers: Arc<HashMap<(Path, Method), Arc<dyn HeliumTask + Sync + Send>>>
 }
 
@@ -23,8 +26,12 @@ impl Server {
         let mut server = Server {
             busy_threads: 0,
             max_threads,
+
             thread_pool: Vec::new(),
             task_queue,
+            streams: HashMap::new(),
+            channel_rx: None,
+
             handlers: Arc::new(HashMap::new())
         };
 
@@ -38,9 +45,23 @@ impl Server {
     ///
     fn create_thread_pool(&mut self, thread_cap: u32) {
 
+        /* Helium multithreaded design philosophy:
+         *    The main thread receives TCP connections and reads all data from the stream,
+         *    it then adds the TCPStream to a data structure with a key and passes the key
+         *    to a worker thread along with the request content. The worker thread parses 
+         *    the content and generates a response, before sending the response back to the 
+         *    main thread with the corresponding key. The main thread then sends the content 
+         *    over the TCPStream.
+        */
+
+        let (tx, rx) = channel::<(u32, String)>();
+        self.channel_rx = Some(rx);
+
+
         for _ in 0..thread_cap {
             let task_mutex = Arc::clone(&self.task_queue);
             let map = Arc::clone(&self.handlers);
+            let tx = tx.clone();
 
             self.thread_pool.push(thread::spawn(move || {
                 /* Thread task handler */
@@ -52,50 +73,32 @@ impl Server {
                     let mut tasks = lock_res.unwrap();
                     
                     // Handle the first task in the queue
-                    let mut stream = match tasks.pop_front() {
+                    let (stream_key, request_string) = match tasks.pop_front() {
                         None => continue,
                         Some(s) => s
                     };
 
-
-                    let mut data = String::new();
-                    let read_res = stream.read_to_string(&mut data);
-                    if read_res.is_err() {
-                        continue;
-                    }
-                    let req = match parse_http_request(&data) {
+                    let req = match parse_http_request(&request_string) {
                         Ok(r) => r,
                         Err(e) => {
-
-                            println!("There was an error parsing the request: {:?}", e);
-
                             let response = http::create_response(/* TODO: add error spec to args */);
-                            let _ = stream.write(response.as_bytes());
+                            tx.send((stream_key, response));
                             continue;
                         }
                     };
 
-                    println!("Received request: {:#?}", req);
-
-                    // - Lookup corresponding HeliumTask
+                    // Lookup corresponding HeliumTask
                     let task = match map.get(&(req.path, req.method)) {
                         None => {
-
-                            // TEMP TEST RESPONSE
-                            let response_content = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nTest";
-                            eprintln!("Response:\n\n{}\n\n",response_content);
-                            let res = stream.write_all(response_content.as_bytes());
-                            stream.flush().expect("test");
-                            eprintln!("Write result: {:?}", res);
-
-
+                            // Send 404 response
+                            tx.send((stream_key, "404 test".to_string()));
                             continue;
                         },
                         Some(t) => t
                     };
-                    // - Execute task 
+                    // Execute task 
                     let response = task.execute();
-
+                    
 
                     // Return response over TcpStream
                     let status = response.status;
@@ -105,9 +108,7 @@ impl Server {
                     };
                         
                     // Format HTTP response
-                    let r = stream.write_all("Hello!".as_bytes());
-                    stream.flush().expect("Error flushing stream");
-                    eprintln!("Write result: {:?}", r);
+                    tx.send((stream_key, "Test success!".to_string()));
                     
                 }
             }));
@@ -126,29 +127,78 @@ impl Server {
 
 
         let listener = TcpListener::bind(socket_addr)?;
+        listener.set_nonblocking(true).expect("Couldn't initialise non-blocking TCP listener.");
         
-
         // Accept incoming connections
-        for stream in listener.incoming() {
+        loop {
+            // Non-blocking accept
+            let stream = listener.accept();
+
             match stream {
                 Err(_) => continue,
-                Ok(s) => {
-
+                Ok(mut s) => {
+                    eprintln!("Received: {:?}", s);
                     // Add TCP stream to task queue for handling by worker threads
 
                     let lock_res = self.task_queue.lock();
                     if lock_res.is_err() {
                         continue;
                     }
-                    let mut streams = lock_res.unwrap();
+                    let mut tasks = lock_res.unwrap();
 
-                    streams.push_back(s);
+                    let mut request_bytes: Vec<u8> = Vec::new();
+                    let read_res = s.0.read(&mut request_bytes);
+
+                    if read_res.is_err() {
+                        eprintln!("[ERROR]    Couldn't read content from TCPStream.");
+                        eprintln!("{:?}", read_res);
+                        continue;
+                    }
+
+                    let stream_key = rand::random::<u32>();
+
+                    // Log stream in hashmap
+                    self.streams.insert(stream_key, s.0);
+                    // Pass content and stream key to worker threads
+                    let request_string = match String::from_utf8(request_bytes) {
+                        Ok(s) => s,
+                        Err(_) => continue
+                    };
+                    tasks.push_back((stream_key, request_string));
                 }
             }
-        }   
 
-        todo!();
+            // Now try to receive any messages from the communication channels
+            match &self.channel_rx {
+                None => continue,
+                Some(rx) => {
+                    // Non-blocking receive attempt
+                    let maybe_data = rx.try_recv();
+                    if maybe_data.is_err() {
+                        continue;
+                    }
+                    
+                    let data = maybe_data.expect("Channel data errored unexpectedly.");
+                    
+                    let maybe_stream = self.streams.remove(&data.0);
+                    if let Some(mut outgoing_stream) = maybe_stream {
+                        let write_res = outgoing_stream.write(data.1.as_bytes());
+
+                        if write_res.is_err() {
+                            eprintln!("Failed to write data over socket.");
+                            continue;
+                        }
+                        
+                        // Now close the stream
+                        let _ = outgoing_stream.shutdown(std::net::Shutdown::Both);
+                    }
+
+                }
+            }
+
+        }
     }
+
 }
 
 
